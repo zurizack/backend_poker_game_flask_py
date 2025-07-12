70917180dbcb570439ca543ae64bb1f604bb0fbd
unknown <zur2005@gmail.com>
2025-07-12 06:42:21 +0300

    add logging for init game

--- poker_server/game/__init__.py
+++ poker_server/game/__init__.py
@@ -1,7 +1,15 @@
   # poker_server/game/__init__.py
+import logging
+logging.info("Inside register_game_blueprints function.")
+from .routes.poker_tables import poker_tables_bp
+from .routes.table_players import table_players_bp
+
+
 def register_game_blueprints(app):
-    from .routes.poker_tables import poker_tables_bp
-    from .routes.table_players import table_players_bp
 
     app.register_blueprint(poker_tables_bp)
+    logging.info(f"Blueprint 'poker_tables_bp' registered. URL prefix: {poker_tables_bp.url_prefix}") 
+
     app.register_blueprint(table_players_bp)
+    logging.info(f"Blueprint 'table_players_bp' registered. URL prefix: {table_players_bp.url_prefix}") 
+
