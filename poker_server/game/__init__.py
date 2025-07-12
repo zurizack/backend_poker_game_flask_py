060fd11b1d1d8d9d0af71c8b83c77cdb1b2f65a0
unknown <zur2005@gmail.com>
2025-07-12 05:53:43 +0300

    clear un nececery things

--- poker_server/game/__init__.py
+++ poker_server/game/__init__.py
@@ -1,7 +1,4 @@
   # poker_server/game/__init__.py
-
-from flask import Blueprint
-
 def register_game_blueprints(app):
     from .routes.poker_tables import poker_tables_bp
     from .routes.table_players import table_players_bp
